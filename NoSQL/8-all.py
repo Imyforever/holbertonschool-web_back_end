476e26a57b9df16e9c3b5ea3b654553d5d799477
Imyforever <109327496+Imyforever@users.noreply.github.com>
2026-02-06 22:08:55 -0500

    Update 8-all.py

--- NoSQL/8-all.py
+++ NoSQL/8-all.py
@@ -1,3 +1,5 @@
 #!/usr/bin/env python3
+""" List all documents in Python """
 def list_all(mongo_collection):
+    """Lists all documents in a collection"""
     return list(mongo_collection.find())
